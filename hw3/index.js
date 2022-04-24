4e8c6c0a452beb77da915d5fc0a673c71ffad57b
NSLyuma <iinokayo@mail.ru>
2022-04-24 17:54:05 +0300

    Little changes in hw3

--- hw3/index.js
+++ hw3/index.js
@@ -4,19 +4,19 @@ const readline = require("readline");
 const IP_89 = "89.123.1.41";
 const IP_34 = "34.48.240.111";
 
-const IP_89_LOG = "./89.123.1.41_requests.log";
-const IP_34_LOG = "./34.48.240.111_requests.log";
+const IP_89_LOG = `./${IP_89}_requests.log`;
+const IP_34_LOG = `./${IP_34}_requests.log`;
 const ACCESS_LOG = "./access.log";
 
-const ip89Data = fs.createWriteStream(IP_89_LOG, {
-  encoding: "utf-8",
-  flags: "a",
-});
+const createIpWriteStream = (fileName) => {
+  return fs.createWriteStream(fileName, {
+    encoding: "utf-8",
+    flags: "a",
+  });
+};
 
-const ip34Data = fs.createWriteStream(IP_34_LOG, {
-  encoding: "utf-8",
-  flags: "a",
-});
+const ip89Data = createIpWriteStream(IP_89_LOG);
+const ip34Data = createIpWriteStream(IP_34_LOG);
 
 const readStream = fs.createReadStream(ACCESS_LOG, {
   encoding: "utf-8",
@@ -36,3 +36,5 @@ readInterface.on("line", (line) => {
     ip34Data.write(line + "\n");
   }
 });
+
+// node index.js
