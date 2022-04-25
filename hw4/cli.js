07f3c8d6a2703d29efbe42fd053b8c9ee923a635
NSLyuma <iinokayo@mail.ru>
2022-04-25 15:15:52 +0300

    Some changes in hw4

--- hw4/cli.js
+++ hw4/cli.js
@@ -1,51 +1,72 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const readLine = require("readline");
 const inquirer = require("inquirer");
+const yargs = require("yargs");
+const { lstatSync } = require("fs");
 
 const rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const readUserFile = (fullPath) => {
-  const fileList = fs.readdirSync(fullPath);
-
-  inquirer
-    .prompt([
-      {
-        name: "fileName",
-        type: "list", //checkbox, confirm, input, list, number, password
-        message: "Выберите файл для чтения",
-        choices: fileList,
-      },
-    ])
-    .then((answer) => {
-      console.log(answer);
-    });
-};
-
-const readDirectory = (fullPath) => {
-  fs.readdir(fullPath, (err) => {
-    if (err) {
-      console.log("ОШИБКА: Папка не найдена!");
-    } else {
-      readUserFile(fullPath);
-    }
-  });
-};
-
 const question = (questionString) =>
   new Promise((resolve) => {
     rl.question(questionString, resolve);
   });
 
-(async () => {
-  const filePath = await question("Введите путь к папке: ");
+const options = yargs
+  .positional("d", {
+    describe: "Path to directory",
+    default: process.cwd(),
+  })
+  .positional("p", {
+    describe: "Pattern",
+    default: null,
+  }).argv;
+console.log(options);
 
-  const fullPath = path.join(__dirname, filePath);
+class ListItem {
+  constructor(path, fileName) {
+    this.path = path;
+    this.fileName = fileName;
+  }
 
-  readDirectory(fullPath);
+  get isDir() {
+    return lstatSync(this.path).isDirectory();
+  }
+}
 
-  rl.close();
-})();
+const start = async () => {
+  let filePath = await question("Введите путь к папке: ");
+  const list = await fs.readdir(filePath);
+  const items = list.map(
+    (fileName) => new ListItem(path.join(filePath, fileName), fileName)
+  );
+
+  const item = await inquirer
+    .prompt([
+      {
+        name: "fileName",
+        type: "list",
+        message: `Выбранная папка/файл: ${filePath}`,
+        choices: items.map((item) => ({ name: item.fileName, value: item })),
+      },
+    ])
+    .then((answer) => answer.fileName);
+
+  if (item.isDir) {
+    filePath = item.path;
+    return await start();
+  } else {
+    const data = await fs.readFile(item.path, "utf-8");
+
+    if (options.p == null) console.log(data);
+    else {
+      const regExp = new RegExp(options.p, "igm");
+      console.log(data.match(regExp));
+    }
+  }
+};
+
+start();
